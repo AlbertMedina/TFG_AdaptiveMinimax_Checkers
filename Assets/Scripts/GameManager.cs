3e8fcb9495c806e313572851fc3751d30e7aeab9
albertmc36 <albertmecu@gmail.com>
2021-04-19 11:53:33 +0200

    Algorithm class

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Game : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     [Header("Prefabs")]
     [SerializeField] GameObject board;
@@ -16,11 +16,7 @@ public class Game : MonoBehaviour
 
     private enum GameMode { Player_Black, Player_White, AI_vs_AI }
 
-    private struct AvailableMove
-    {
-        public Move move;
-        public float score;
-    }
+    
 
     private Board gameBoard;
 
@@ -62,7 +58,7 @@ public class Game : MonoBehaviour
                     {
                         if (Input.GetKeyDown(KeyCode.Space))
                         {
-                            AvailableMove chosenMove = Minimax(gameBoard, gameBoard.turn, 0, 3);
+                            Algorithm.AvailableMove chosenMove = Algorithm.Minimax(gameBoard, gameBoard.turn, 0, 3);
 
                             if (chosenMove.move == null)
                             {
@@ -86,7 +82,7 @@ public class Game : MonoBehaviour
 
                     if (Input.GetKeyDown(KeyCode.Space))
                     {
-                        AvailableMove chosenMove = Minimax(gameBoard, gameBoard.turn, 0, 3);
+                        Algorithm.AvailableMove chosenMove = Algorithm.Minimax(gameBoard, gameBoard.turn, 0, 3);
 
                         if (chosenMove.move == null)
                         {
@@ -298,62 +294,5 @@ public class Game : MonoBehaviour
         {
             Destroy(board.transform.GetChild(jumped.y).GetChild(jumped.x).GetChild(0).gameObject);
         }
-    }
-
-    AvailableMove Minimax(Board _board, Board.Turn _turn, int _currentDepth, int _maxDepth)
-    {
-        AvailableMove bestMove = new AvailableMove();
-
-        bestMove.move = null;
-
-        if (_currentDepth >= _maxDepth)
-        {
-            bestMove.score = _board.Evaluate(_turn);
-            return bestMove;
-        }
-
-        List<Move> availableMoves = _board.GetAllMoves();
-
-        if (availableMoves.Count == 0)
-        {
-            bestMove.score = _board.Evaluate(_turn);
-            return bestMove;
-        }
-
-        if (_turn == _board.turn) bestMove.score = -Mathf.Infinity;
-        else bestMove.score = Mathf.Infinity;
-
-        AvailableMove currentMove = new AvailableMove();
-
-        foreach (Move m in availableMoves)
-        {
-            Board newBoard;
-
-            if (_board.turn == Board.Turn.Black) newBoard = new Board((Board.Square[,])_board.boardState.Clone(), Board.Turn.White);
-            else newBoard = new Board((Board.Square[,])_board.boardState.Clone(), Board.Turn.Black);
-
-            newBoard.MakeMove(m);
-            
-            currentMove = Minimax(newBoard, _turn, _currentDepth + 1, _maxDepth);
-            
-            if (_turn == _board.turn)
-            {
-                if (currentMove.score > bestMove.score)
-                {
-                    bestMove.score = currentMove.score;
-                    bestMove.move = m;
-                }
-            }
-            else
-            {
-                if (currentMove.score < bestMove.score)
-                {
-                    bestMove.score = currentMove.score;
-                    bestMove.move = m;
-                }
-            }
-        }
-
-        return bestMove;
     }
 }
